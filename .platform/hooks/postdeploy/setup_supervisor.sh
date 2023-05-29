5a0860cbbbcee8467789b4ddab35ddadeed46517
lhenriquez <luis.henriquez@wearemo.com>
2023-05-29 15:47:37 -0500

    fix supervisor

--- .platform/hooks/postdeploy/setup_supervisor.sh
+++ .platform/hooks/postdeploy/setup_supervisor.sh
@@ -1,5 +1,52 @@
 #!/usr/bin/env bash
 # ref: https://hackernoon.com/setting-up-django-channels-on-aws-elastic-beanstalk-716fd5a49c4a
+#!/bin/bash
+
+# If you want to have more than one application, and in just one of them to run the supervisor, uncomment the lines below, 
+# and add the env variable IS_WORKER as true in the EBS application you want the supervisor
+
+#if [ "${IS_WORKER}" != "true" ]; then
+#    echo "Not a worker. Set variable IS_WORKER=true to run supervisor on this instance"
+#    exit 0
+#fi
+
+echo "Supervisor - starting setup"
+. /opt/elasticbeanstalk/deployment/env
+
+
+if [ ! -f /usr/local/bin/supervisord ]; then
+    echo "installing supervisor"
+    sudo yum install pip
+    sudo pip install supervisor
+else
+    echo "supervisor already installed"
+fi
+
+if [ ! -d /etc/supervisor ]; then
+    mkdir /etc/supervisor
+    echo "create supervisor directory"
+fi
+
+if [ ! -d /etc/supervisor/conf.d ]; then
+    mkdir /etc/supervisor/conf.d
+    echo "create supervisor configs directory"
+fi
+
+#. /opt/elasticbeanstalk/deployment/env && cat .ebextensions/supervisor/supervisord.conf > /etc/supervisor/supervisord.conf
+#. /opt/elasticbeanstalk/deployment/env && cat .ebextensions/supervisor/supervisord.conf > /etc/supervisord.conf
+#. /opt/elasticbeanstalk/deployment/env && cat .ebextensions/supervisor/supervisor_laravel.conf > /etc/supervisor/conf.d/supervisor_laravel.conf
+
+if ps aux | grep "[/]usr/local/bin/supervisord"; then
+    echo "supervisor is running"
+else
+    echo "starting supervisor"
+    /usr/local/bin/supervisord
+fi
+
+# /usr/local/bin/supervisorctl reread
+# /usr/local/bin/supervisorctl update
+
+echo "Supervisor Running!"
 
 # Get django environment variables
 djangoenv=`export | tr '\n' ',' | sed 's/%/%%/g' | sed 's/export //g' | sed 's/$PATH/%(ENV_PATH)s/g' | sed 's/$PYTHONPATH//g' | sed 's/$LD_LIBRARY_PATH//g'`
