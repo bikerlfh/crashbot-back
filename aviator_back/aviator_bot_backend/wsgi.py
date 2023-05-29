58e838b543da1c6cb88bd59651e42f1fda185d2c
lhenriquez <luis.henriquez@wearemo.com>
2023-05-28 21:47:10 -0500

    update ebextensions

--- aviator_back/aviator_bot_backend/wsgi.py
+++ aviator_back/aviator_bot_backend/wsgi.py
@@ -13,6 +13,6 @@ import os
 # Django
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aviator_bot_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aviator_bot_backend.settings.development")
 
 application = get_wsgi_application()
